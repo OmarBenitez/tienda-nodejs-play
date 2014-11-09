54c14f8e8aecdb434eb8fd2ad3829bf3b060ab96
Zacht21 <jhermosillo.sa@gmail.com>
2014-11-08 23:18:22 -0700

    SubTipo

--- app/models/enums/SubTipo.java
+++ app/models/enums/SubTipo.java
@@ -4,17 +4,33 @@ package models.enums;
  * @author Zacht
  */
 public enum SubTipo {
-    
-    COMPUTO(TipoProducto.ELECTRONICA, "Tv's"),
-    VIDEOJUEGOS(TipoProducto.ELECTRONICA, "Audio");
-    
-    public TipoProducto papa;
-    
-    public String descripcion;
 
-    private SubTipo(TipoProducto papa, String descripcion) {
+    TVS(TipoProducto.ELECTRONICA, "Tv's"),
+    COMPUTO(TipoProducto.ELECTRONICA, "Computadora"),
+    VIDEOJUEGOS(TipoProducto.ELECTRONICA, "Video Juegos"),
+    CAMARAS(TipoProducto.ELECTRONICA, "Camaras"),
+    CDS(TipoProducto.ELECTRONICA, "CD's"),
+
+    CAMISAS(TipoProducto.ROPA, "Caisas"),
+    BLUSAS(TipoProducto.ROPA, "Blusas"),
+    PANTALONES(TipoProducto.ROPA, "Pantalones"),
+    SHORTS(TipoProducto.ROPA, "Shorts"),
+    INTERIOR(TipoProducto.ROPA, "Ropa Interior"),
+
+    COCINA(TipoProducto.LINEABLANCA, "Cocina"),
+    LAVADO(TipoProducto.LINEABLANCA, "Lavado"),
+    REFRIGERADOR(TipoProducto.LINEABLANCA, "Refrigeradores"),
+    UTENSILIOS(TipoProducto.LINEABLANCA, "Utensilios"),
+    MICROONDAS(TipoProducto.LINEABLANCA, "Microondas");
+
+
+    public String descripcion;
+    public TipoProducto papa;
+
+    SubTipo (TipoProducto papa, String descripcion){
         this.papa = papa;
         this.descripcion = descripcion;
     }
-    
+
 }
+
